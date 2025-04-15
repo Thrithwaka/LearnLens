3151d6478c8cfada2862c3594b1da553ae7be911
Thrithwaka <thrithwakapreethi57@gmail.com>
2025-04-15 15:30:40 +0530

    Fix: Bind Flask app to 0.0.0.0 and support custom port via PORT env variable

--- app.py
+++ app.py
@@ -2155,4 +2155,5 @@ with app.app_context():
 
 # Run the app
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 10000))
+    app.run(debug=True, host='0.0.0.0', port=port)
